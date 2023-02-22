2262894b1b1ccb3df20097cf4ea9b69bc9e6ad62
brahiiim <brahim.abdelli994@gmail.com>
2023-02-22 22:09:55 +0100

    adding swagger

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,18 @@
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.setGlobalPrefix('api');
+
+  const options = new DocumentBuilder()
+    .setTitle('Smurfit Backend')
+    .setDescription('Backend for the Smurfit project')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, options);
+  SwaggerModule.setup('/docs', app, document);
   await app.listen(3000);
 }
 bootstrap();

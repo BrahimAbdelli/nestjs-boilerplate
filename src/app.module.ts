1eb5931b636317cdfc14810baf981ceef1340d5b
brahiiim <brahim.abdelli994@gmail.com>
2023-02-25 22:34:47 +0100

    Adding dependencies and app configuration

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,30 @@
-import { Module } from '@nestjs/common';
+import { CacheModule, Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import { UsersModule } from './modules/users/users.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true
+    }),
+    CacheModule.register(),
+    TypeOrmModule.forRoot({
+      type: 'mongodb',
+      host: process.env.DB_HOST,
+      database: process.env.DB_NAME,
+      entities: ['src/modules/*/**.entity{.ts,.js}'],
+      synchronize: false,
+      logger: 'advanced-console',
+      useUnifiedTopology: true,
+      autoLoadEntities: true
+    }),
+
+    UsersModule
+  ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService]
 })
 export class AppModule {}

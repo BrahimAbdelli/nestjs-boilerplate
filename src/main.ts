c4d3139b432810812c7e92a4486be2c2a29425dc
brahiiim <brahim.abdelli994@gmail.com>
2023-02-25 22:34:51 +0100

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,17 @@
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { NestExpressApplication } from '@nestjs/platform-express';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule, {
+    cors: true
+  });
   app.setGlobalPrefix('api');
 
   const options = new DocumentBuilder()
-    .setTitle('Smurfit Backend')
-    .setDescription('Backend for the Smurfit project')
+    .setTitle('Nestjs Boilerplate')
+    .setDescription('This is a project aimed to be a nestjs boilerplate')
     .setVersion('1.0')
     .build();
   const document = SwaggerModule.createDocument(app, options);

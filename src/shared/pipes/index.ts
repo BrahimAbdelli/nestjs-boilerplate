606084fc3afed6237f619c002adfb954c1449b2e
brahiiim <brahim.abdelli994@gmail.com>
2023-04-22 15:38:42 +0200

    Adding abstract-validation pipe to ensure class validation for generics.

--- src/shared/pipes/index.ts
+++ src/shared/pipes/index.ts
@@ -1,2 +1,3 @@
 export { ValidateObjectIdPipe } from './validate-object-id.pipe';
 export { ValidationPipe } from './validation.pipe';
+export { AbstractValidationPipe } from './abstract-validation.pipe';

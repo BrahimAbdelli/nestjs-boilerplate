95a0904418f4f535d5a96949bb9eda92ed3353c6
brahiiim <brahim.abdelli994@gmail.com>
2023-05-05 22:17:03 +0200

    Cleaning category module.

--- src/modules/category/category.controller.ts
+++ src/modules/category/category.controller.ts
@@ -1,26 +1,9 @@
-import { ObjectID } from 'mongodb';
-import { ValidateObjectIdPipe } from '../../shared/pipes/validate-object-id.pipe';
-import { CategoryService } from './category.service';
-import { InjectRepository } from '@nestjs/typeorm';
-import { ApiTags, ApiBody } from '@nestjs/swagger';
-import {
-  Controller,
-  Injectable,
-  Post,
-  Body,
-  Get,
-  Param,
-  Delete,
-  Put,
-  Patch,
-  Query,
-  ParseBoolPipe
-} from '@nestjs/common';
-import { CategoryEntity } from './entities/category.entity';
-import { Repository } from 'typeorm';
-import { findByField } from '../../shared/find-by-field.utils';
+import { Controller } from '@nestjs/common';
+import { ApiTags } from '@nestjs/swagger';
 import { BaseController } from '../../shared/base/base.controller';
+import { CategoryService } from './category.service';
 import { CategoryCreateDto, CategoryUpdateDto } from './dtos';
+import { CategoryEntity } from './entities/category.entity';
 
 @Controller('categories')
 @ApiTags('categories')
@@ -31,41 +14,4 @@ export class CategoryController extends BaseController<CategoryEntity, CategoryC
   constructor(private readonly categoryService: CategoryService) {
     super(categoryService);
   }
-
-  /*   @Post()
-  @ApiBody({ type: [NewsletterCreateDto] })
-  async createNewsletter(@Body() newsletterData: NewsletterCreateDto) {
-    return await this.newsletterService.createNewsletter(newsletterData);
-  } */
-
-  /*   @Get()
-  //@Roles(adminRoles.admin)
-  async findAll(): Promise<CategoryEntity[]> {
-    return await this.newsletterService.findAll(false);
-  } */
-
-  //This one overrides the one in basecontroller
-  /*   @Get(':id')
-  //@Roles(adminRoles.admin)
-  @ApiBody({ description: 'id', required: true })
-  async findOne(@Param(new ValidateObjectIdPipe('Newsletter')) params): Promise<CategoryEntity> {
-    // throws error 404 if not found
-    const newsletter = await findByField(this.newsletterRepository, { id: params.id }, true);
-    return newsletter;
-  } */
-
-  /*   @Put(':id')
-  //@Roles(adminRoles.admin)
-  @ApiBody({ type: [CategoryUpdateDto] })
-  async update(@Param(new ValidateObjectIdPipe('Category')) params, @Body() categoryData: CategoryUpdateDto) {
-    // Check if entity exists  throws exception if not exists!
-    const toUpdate = await findByField(this.categoryRepository, { _id: params.id }, true);
-    return await this.categoryService.updateCategory(toUpdate, categoryData);
-  } */
-
-  /*   @Patch(':id')
-  //@Roles(adminRoles.admin)
-  async deleteLogic(@Param(new ValidateObjectIdPipe('Category')) params) {
-    return await this.categoryService.deleteLogic(new ObjectID(params.id));
-  } */
 }
--- src/modules/category/category.service.ts
+++ src/modules/category/category.service.ts
@@ -1,16 +1,11 @@
-import { HttpException } from '@nestjs/common/exceptions/http.exception';
-import { REQUEST } from '@nestjs/core';
-import { IGetUserAuthInfoRequest } from '../../shared/user-request.interface';
-import { ObjectID } from 'mongodb';
-import { InjectRepository } from '@nestjs/typeorm';
 import { Inject, Injectable } from '@nestjs/common';
-import { CategoryEntity } from './entities/category.entity';
-import { DeleteResult, Repository } from 'typeorm';
-import { cateogryStates } from './entities/category.state.enum';
-import { throwError } from '../../shared/throw-error.utils';
-import { findByField } from '../../shared/find-by-field.utils';
+import { REQUEST } from '@nestjs/core';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
 import { BaseService } from '../../shared/base/base.service';
+import { IGetUserAuthInfoRequest } from '../../shared/user-request.interface';
 import { CategoryCreateDto, CategoryUpdateDto } from './dtos';
+import { CategoryEntity } from './entities/category.entity';
 
 @Injectable()
 export class CategoryService extends BaseService<CategoryEntity, CategoryCreateDto, CategoryUpdateDto> {
@@ -20,48 +15,5 @@ export class CategoryService extends BaseService<CategoryEntity, CategoryCreateD
     @Inject(REQUEST) public readonly request: IGetUserAuthInfoRequest
   ) {
     super(categoryRepository, request);
-  }
-
-  async createCategory(dto: CategoryCreateDto): Promise<void> {
-    const newCategory = Object.assign(new CategoryEntity(), dto);
-    console.log('inside else');
-    newCategory.isDeleted = false;
-    newCategory.state = cateogryStates.waiting;
-    try {
-      await this.categoryRepository.save(newCategory);
-    } catch (error) {
-      if (error?.code === 11000) {
-        throwError({ TagEntity: 'Duplicated value' }, 'Value exists', 405);
-      }
-      throw new Error('Something went wrong');
-    }
-  }
-  async findById(id: string): Promise<CategoryEntity> {
-    return await findByField(this.categoryRepository, { _id: id }, true);
-  }
-
-  /*   async findAll(isDeleted: boolean): Promise<CategoryEntity[]> {
-    return await this.categoryRepository.find({ isDeleted: isDeleted });
-  } */
-
-  async updateCategory(toUpdate: CategoryEntity, dto: CategoryUpdateDto): Promise<CategoryEntity> {
-    toUpdate.userUpdated = this.request.user._id;
-    Object.assign(toUpdate, dto);
-    try {
-      return await this.categoryRepository.save(toUpdate);
-    } catch (error) {
-      if (error?.code === 11000) {
-        throwError({ TagEntity: 'Duplicated value' }, 'Value exists', 405);
-      }
-      throw new Error('Something went wrong');
-    }
-  }
-
-  async deleteLogic(_id: ObjectID): Promise<CategoryEntity> {
-    return await this.categoryRepository.save({
-      _id: _id,
-      isDeleted: true,
-      userUpdated: this.request.user._id
-    });
   }
 }

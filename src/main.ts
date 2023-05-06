56a71530f6cf82f573bf0e2ba96608d8f2499643
brahiiim <brahim.abdelli994@gmail.com>
2023-05-06 20:54:22 +0200

    Adding compression, rate limiting, helmet and serve favicon configuration.

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,15 @@
-import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
-import { NestFactory, Reflector } from '@nestjs/core';
-import { AppModule } from './app.module';
-import { NestExpressApplication } from '@nestjs/platform-express';
 import { ClassSerializerInterceptor } from '@nestjs/common';
+import { NestFactory, Reflector } from '@nestjs/core';
+import { NestExpressApplication } from '@nestjs/platform-express';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { AppModule } from './app.module';
 import { ValidationPipe } from './shared/pipes';
+
+import * as compression from 'compression';
+import rateLimit from 'express-rate-limit';
+import helmet from 'helmet';
+import { join } from 'path';
+import * as favicon from 'serve-favicon';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule, {
@@ -21,6 +27,15 @@ async function bootstrap() {
   app.useGlobalInterceptors(new ClassSerializerInterceptor(new Reflector()));
   //app.useGlobalGuards(new RolesGuard(new Reflector()));
   app.useGlobalPipes(new ValidationPipe());
-  await app.listen(3000);
+  app.use(
+    compression(),
+    helmet(),
+    rateLimit({
+      windowMs: 15 * 60 * 1000, // 15 minutes
+      max: 10000 // limit each IP to 100 requests per windowMs
+    }),
+    favicon(join(__dirname, '..', 'public', 'favicon.ico'))
+  );
+  await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
